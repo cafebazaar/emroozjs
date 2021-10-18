ecb0c4f21003dcc2f2be3c879d3461eeabe77e7a
Pouya <pouya.mozafar@gmail.com>
2021-10-18 14:38:03 +0330

    Add conditional build config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,28 @@ import dts from 'vite-plugin-dts';
 
 const resolve = (addr: string) => path.resolve(__dirname, addr);
 
+const buildConfig = process.env.NODE_ENV === 'STORYBOOK_BUILD' ? {
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/index.ts'),
+      name: 'Emrooz',
+      fileName: (format: string) => `emrooz.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+} : {};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), dts()],
@@ -28,23 +50,5 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'lib/index.ts'),
-      name: 'Emrooz',
-      fileName: (format) => `emrooz.${format}.js`,
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
+  ...buildConfig,
 });
