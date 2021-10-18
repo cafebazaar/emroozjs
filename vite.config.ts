ac5eb3dc930de92473bd927df284ec816445827b
Pouya <pouya.mozafar@gmail.com>
2021-10-18 20:52:06 +0330

    Remove vue-tsc for now beacuse of the conflict

--- vite.config.ts
+++ vite.config.ts
@@ -50,5 +50,23 @@ export default defineConfig({
       },
     },
   },
-  ...buildConfig,
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/index.ts'),
+      name: 'Emrooz',
+      fileName: (format: string) => `emrooz.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 });
