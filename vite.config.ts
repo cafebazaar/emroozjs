85bada3d836ac8c40bdf3423c206391f3b55a0c4
Pouya <pouya.mozafar@gmail.com>
2021-10-18 22:51:33 +0330

    Fix build config

--- vite.config.ts
+++ vite.config.ts
@@ -47,23 +47,5 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'lib/index.ts'),
-      name: 'Emrooz',
-      fileName: (format: string) => `emrooz.${format}.js`,
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
+  ...buildConfig,
 });
