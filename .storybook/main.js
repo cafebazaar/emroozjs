ed1c5eb9b88f32ae711a7a2b24f835815f27c3a8
Pouya <pouya.mozafar@gmail.com>
2021-10-18 12:35:06 +0330

    Add initial storybook

---
+++ .storybook/main.js
@@ -0,0 +1,28 @@
+const { loadConfigFromFile, mergeConfig } = require("vite");
+const path = require('path')
+
+
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "core": {
+    "builder": "storybook-builder-vite"
+  },
+  async viteFinal(config, { configType }) {
+    const { config: userConfig } = await loadConfigFromFile(
+      path.resolve(__dirname, "../vite.config.ts")
+    );
+
+    return mergeConfig(config, {
+      ...userConfig,
+      // manually specify plugins to avoid conflict
+      plugins: [],
+    });
+  },
+}
