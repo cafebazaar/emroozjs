91d88b77248fdff7f312a28c16d586b88c448509
Pouya <pouya.mozafar@gmail.com>
2021-10-19 15:38:15 +0330

    Change logo of storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,10 @@
+// .storybook/YourTheme.js
+
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: '@cafebazaar/emrooz',
+  brandUrl: 'https://github.com/cafebazaar/emroozjs',
+  brandImage: '/assets/storybook-logo.png',
+});
